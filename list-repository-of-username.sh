b1b19bb50f5f60068018199c7b0075492b0f6b13
Samarth Gupta <102611901+Samarth622@users.noreply.github.com>
2024-06-19 18:37:46 +0530

    Add files via upload
    shell for getting all repositories of the user or organization

---
+++ list-repository-of-username.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+######################################
+# About: This script lists out the repositories that are currently present in the specified GitHub organization or user profile.
+# Required: First export username and access token before running the script and provide one argument when running the script.
+#           First argument : Owner name (organization or user)
+# Author: Samarth Gupta
+# Date: 19-06-2024
+######################################
+
+# Github API url
+API_URL="https://api.github.com"
+
+# Check if username and token are set
+if [[ -z "$username" || -z "$token" ]]; then
+  echo "Error: Please export 'username' and 'token' environment variables."
+  exit 1
+fi
+
+# Username and permission access token
+USERNAME=$username
+TOKEN=$token
+
+# Arguments
+OWNER_NAME=$1
+
+# Check if OWNER_NAME is provided
+if [[ -z "$OWNER_NAME" ]]; then
+  echo "Error: Please provide the owner name (organization or user) as an argument."
+  exit 1
+fi
+
+# Function to make a GET request to Github API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list repositories in the organization or user profile
+function list_repositories {
+    local endpoint="users/${OWNER_NAME}/repos" # Change this to "orgs/${OWNER_NAME}/repos" for organizations
+
+    repositories="$(github_api_get "${endpoint}" | jq -r '.[] | .name')"
+
+    if [[ -z "$repositories" ]]; then
+        echo "No repositories found for ${OWNER_NAME}."
+    else 
+        echo "Repositories for ${OWNER_NAME}:"
+        echo "$repositories"
+    fi
+}
+
+echo "Listing repositories for ${OWNER_NAME}..."
+list_repositories
